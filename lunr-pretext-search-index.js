1c016cbd7b4ee18d909188b43aeef164cd2df370
Cameron Geisler <ckg888888@gmail.com>
2025-03-05 13:22:40 -0800

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,25 +16,115 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.2",
   "title": "Inverse Trigonometric Substitution",
-  "body": " Inverse Trigonometric Substitution   Inverse trigonometric substitution is a technique used to evaluate integrals that contain expressions of the form \\( \\sqrt{a^2 - x^2} \\), \\( \\sqrt{x^2 - a^2} \\), or \\( \\sqrt{a^2 + x^2} \\). This method utilizes trigonometric identities to simplify these expressions.    Baisc Trigonometric Substitutions   Basic Trigonometric Substitutions     For \\( \\sqrt{a^2 - x^2} \\), use \\( x = a \\sin \\theta \\).  For \\( \\sqrt{x^2 - a^2} \\), use \\( x = a \\sec \\theta \\).  For \\( \\sqrt{a^2 + x^2} \\), use \\( x = a \\tan \\theta \\).       Example: Evaluating \\( \\int \\frac{dx}{\\sqrt{9 - x^2}} \\)   Evaluate the integral \\( I = \\int \\frac{dx}{\\sqrt{9 - x^2}} \\) using inverse trigonometric substitution.     "
+  "body": " Inverse Trigonometric Substitution   Inverse trigonometric substitution is a technique used to evaluate integrals that contain expressions of the form , , or . This method uses trigonometric identities to simplify these integrals.    Summary of Inverse Trigonometric Substitution                                    Identify the form of the substitution (sine, tangent, or secant).  Apply the substitution. Sketch a right triangle and label the sides, to represent the relationships between and .  Simplify the integral.  Integrate with the new variable , using previous strategies, typically a trigonometric integral.  Back-substitute using the inverse trigonometric function to substitute back for .      Examples  Example  Evaluate .   Let , so .           Example  Determine    Example  Determine    Example  Determine     Evaluate     Evaluate     Evaluate     Evaluate     Evaluate     Evaluate     Evaluate     "
 },
 {
-  "id": "sec-inverse-trig-sub-3-2",
+  "id": "summary-of-inverse-trigonometric-substitution-2",
   "level": "2",
-  "url": "sec-inverse-trig-sub.html#sec-inverse-trig-sub-3-2",
-  "type": "Theorem",
+  "url": "sec-inverse-trig-sub.html#summary-of-inverse-trigonometric-substitution-2",
+  "type": "Table",
   "number": "1.2.1",
-  "title": "Basic Trigonometric Substitutions.",
-  "body": " Basic Trigonometric Substitutions     For \\( \\sqrt{a^2 - x^2} \\), use \\( x = a \\sin \\theta \\).  For \\( \\sqrt{x^2 - a^2} \\), use \\( x = a \\sec \\theta \\).  For \\( \\sqrt{a^2 + x^2} \\), use \\( x = a \\tan \\theta \\).     "
+  "title": "",
+  "body": "                            "
 },
 {
-  "id": "sec-inverse-trig-sub-3-3",
+  "id": "examples-2",
   "level": "2",
-  "url": "sec-inverse-trig-sub.html#sec-inverse-trig-sub-3-3",
-  "type": "Example",
+  "url": "sec-inverse-trig-sub.html#examples-2",
+  "type": "Checkpoint",
   "number": "1.2.2",
-  "title": "Example: Evaluating \\( \\int \\frac{dx}{\\sqrt{9 - x^2}} \\).",
-  "body": " Example: Evaluating \\( \\int \\frac{dx}{\\sqrt{9 - x^2}} \\)   Evaluate the integral \\( I = \\int \\frac{dx}{\\sqrt{9 - x^2}} \\) using inverse trigonometric substitution.   "
+  "title": "Example.",
+  "body": "Example  Evaluate .   Let , so .          "
+},
+{
+  "id": "examples-3",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-3",
+  "type": "Checkpoint",
+  "number": "1.2.3",
+  "title": "Example.",
+  "body": "Example  Determine   "
+},
+{
+  "id": "examples-4",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-4",
+  "type": "Checkpoint",
+  "number": "1.2.4",
+  "title": "Example.",
+  "body": "Example  Determine   "
+},
+{
+  "id": "examples-5",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-5",
+  "type": "Checkpoint",
+  "number": "1.2.5",
+  "title": "Example.",
+  "body": "Example  Determine   "
+},
+{
+  "id": "examples-6",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-6",
+  "type": "Checkpoint",
+  "number": "1.2.6",
+  "title": "",
+  "body": " Evaluate   "
+},
+{
+  "id": "examples-7",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-7",
+  "type": "Checkpoint",
+  "number": "1.2.7",
+  "title": "",
+  "body": " Evaluate   "
+},
+{
+  "id": "examples-8",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-8",
+  "type": "Checkpoint",
+  "number": "1.2.8",
+  "title": "",
+  "body": " Evaluate   "
+},
+{
+  "id": "examples-9",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-9",
+  "type": "Checkpoint",
+  "number": "1.2.9",
+  "title": "",
+  "body": " Evaluate   "
+},
+{
+  "id": "examples-10",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-10",
+  "type": "Checkpoint",
+  "number": "1.2.10",
+  "title": "",
+  "body": " Evaluate   "
+},
+{
+  "id": "examples-11",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-11",
+  "type": "Checkpoint",
+  "number": "1.2.11",
+  "title": "",
+  "body": " Evaluate   "
+},
+{
+  "id": "examples-12",
+  "level": "2",
+  "url": "sec-inverse-trig-sub.html#examples-12",
+  "type": "Checkpoint",
+  "number": "1.2.12",
+  "title": "",
+  "body": " Evaluate   "
 }
 ]
 
